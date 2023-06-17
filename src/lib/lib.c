592289e2dc6bb0b52d3e17b2d36a12cea2688682
Srivathsan Sudarsanan <srivathsansudarsanan@outlook.com>
2023-06-17 05:27:45 +0000

    Resolve "Finish the push function"

--- src/lib/lib.c
+++ src/lib/lib.c
@@ -21,18 +21,34 @@ void increase_capacity(vector *input_vector) {
   if ((input_vector->capacity - input_vector->length) > 3) {
     return;
   }
-  input_vector->vec_pointer = realloc(
-      input_vector->vec_pointer, sizeof(int) * input_vector->capacity + 10);
   input_vector->capacity += 10;
+  input_vector->vec_pointer =
+      realloc(input_vector->vec_pointer, sizeof(int) * input_vector->capacity);
 }
 
 void cleanup(vector *input_vector) {
   if ((input_vector->capacity - input_vector->length) < 15) {
     return;
   }
-  input_vector->vec_pointer = realloc(
-      input_vector->vec_pointer, sizeof(int) * input_vector->capacity - 10);
   input_vector->capacity -= 10;
+  input_vector->vec_pointer =
+      realloc(input_vector->vec_pointer, sizeof(int) * input_vector->capacity);
+}
+
+void push(vector *input_vector, int value) {
+  increase_capacity(input_vector);
+  input_vector->vec_pointer[input_vector->length++] = value;
+}
+
+void print_vector(vector *input_vector) {
+  if (input_vector->length == 0) {
+    printf("The vector is empty!\n");
+    return;
+  }
+  for (int i = 0; i < input_vector->length; i++) {
+    printf("%d\t", input_vector->vec_pointer[i]);
+  }
+  printf("\n");
 }
 
 void delete_vector(vector *input_vector) {
--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 int main() {
   vector input_vector = create_vector();
+  for (int i = 0; i < 20; i++) {
+    push(&input_vector, i);
+  }
+  print_vector(&input_vector);
   delete_vector(&input_vector);
   return 0;
 }
